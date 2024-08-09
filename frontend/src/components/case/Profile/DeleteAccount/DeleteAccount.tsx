907665a0fcf128d592e87c5a60fd1ae99b125eb2
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-08-09 15:12:18 +0200

    Minor changes to project structure

--- frontend/src/components/case/Profile/DeleteAccount/DeleteAccount.tsx
+++ frontend/src/components/case/Profile/DeleteAccount/DeleteAccount.tsx
@@ -3,14 +3,18 @@ import deleteAccountRequest from "./DeleteAccountRequest";
 import { useNavigate } from "react-router-dom";
 
 export default function DeleteAccount() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const deleteAccount = () => {
-        console.log("Deleting account");
-        deleteAccountRequest(navigate);
-    }
+  const deleteAccount = () => {
+    console.log("Deleting account");
+    deleteAccountRequest(navigate);
+  };
 
-    return (
-        <Button onClick={deleteAccount} className="mt-custom" variant="outline-danger">Delete account</Button>
-    )
+  return (
+    <div style={{ marginTop: "60%" }}>
+      <Button onClick={deleteAccount} variant="outline-danger">
+        Delete account
+      </Button>
+    </div>
+  );
 }
