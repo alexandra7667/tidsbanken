7b751a6ad6063d7921a33effac43859f4e3ae3cf
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-08-12 09:15:43 +0200

    Added dropdown month, fixed button styling

--- frontend/src/components/case/Dashboard/Dashboard.tsx
+++ frontend/src/components/case/Dashboard/Dashboard.tsx
@@ -1,7 +1,16 @@
 import { useState } from "react";
 import Calendar from "./Calendar/Calendar.tsx";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Container,
+  Dropdown,
+  DropdownButton,
+  Row,
+} from "react-bootstrap";
 import { monthNames } from "../../../assets/strings/monthNames.ts";
+import { CaretLeftFill } from 'react-bootstrap-icons';
+import { CaretRightFill } from 'react-bootstrap-icons';
 
 export default function Dashboard() {
   const now = new Date();
@@ -9,6 +18,7 @@ export default function Dashboard() {
   const currentMonth = now.getMonth(); //0 = January, 1 = February, etc.
   const [year, setYear] = useState(currentYear);
   const [month, setMonth] = useState(currentMonth);
+  const [selectedMonth, setSelectedMonth] = useState(monthNames[currentMonth]); //Title in dropdown
 
   const previousYear = () => {
     setYear(year - 1);
@@ -20,55 +30,77 @@ export default function Dashboard() {
 
   const previousMonth = () => {
     //If january - set december
-    if(month === 0) setMonth(11)
-
-    else setMonth(month - 1);
+    if (month === 0) {
+      setMonth(11);
+      setSelectedMonth(monthNames[11]);
+    }
+    else {
+      setMonth(month - 1);
+      setSelectedMonth(monthNames[month - 1]);
+    }
   };
 
   const nextMonth = () => {
     //if december - set january
-    if(month === 11) setMonth(0)
+    if (month === 11) {
+      setMonth(0);
+      setSelectedMonth(monthNames[0])
+    }
+    else {
+      setMonth(month + 1);
+      setSelectedMonth(monthNames[month + 1]);
+    }
+  };
 
-    else setMonth(month + 1);
+  const handleSelect = (monthName: string) => {
+    setSelectedMonth(monthName);
+    setMonth(monthNames.indexOf(monthName));
   };
 
   const today = () => {
     setMonth(currentMonth);
     setYear(currentYear);
+    setSelectedMonth(monthNames[currentMonth]);
   };
 
   return (
     <>
-      <Container className="mt-4">
+      <Container className="mt-4 mb-2 ">
         <Row className="align-items-center justify-content-center">
-          <Col xs="auto">
-            <Button onClick={previousYear}>Prev</Button>
+          <Col xs="auto" className="px-1">
+            <Button variant="outline-secondary" style={{ display: 'flex' }} onClick={previousYear}><CaretLeftFill /></Button>
           </Col>
-          <Col xs="auto">
+          <Col xs="auto" className="px-1 pt-1">
             <h2>{`${year}`}</h2>
           </Col>
-          <Col xs="auto">
-            <Button onClick={nextYear}>Next</Button>
+          <Col xs="auto" className="px-1">
+            <Button variant="outline-secondary" style={{ display: 'flex' }} onClick={nextYear}><CaretRightFill /></Button>
           </Col>
         </Row>
       </Container>
 
       <Container>
         <Row className="align-items-center justify-content-center">
-          <Col xs="auto">
-            <Button onClick={previousMonth}>Prev</Button>
+          <Col xs="auto" className="px-1">
+            <Button variant="outline-secondary" style={{ display: 'flex' }} onClick={previousMonth}><CaretLeftFill /></Button>
           </Col>
-          <Col xs="auto">
-            <h2>{`${monthNames[month]}`}</h2>
+          <Col xs="auto" className="px-1">
+            <DropdownButton size="lg" id="dropdown-month" title={selectedMonth}>
+              {monthNames.map((monthName, index) => (
+                <Dropdown.Item key={index} onClick={() => handleSelect(monthName)}>
+                  {monthName}
+                </Dropdown.Item>
+              ))}
+            </DropdownButton>
           </Col>
-          <Col xs="auto">
-            <Button onClick={nextMonth}>Next</Button>
+          <Col xs="auto" className="px-1">
+            <Button variant="outline-secondary" style={{ display: 'flex' }} onClick={nextMonth}><CaretRightFill /></Button>
           </Col>
         </Row>
       </Container>
 
       <div className="d-flex justify-content-center m-4">
-        <Button onClick={today}>Today</Button>
+        <Button variant="outline-primary" onClick={today}>Today</Button>
       </div>
 
       <Calendar year={year} month={month} />
